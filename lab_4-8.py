9ef95249d8de811e6bfd63d3299521c5d1448800
Joseph Marotta <p23jmarotta@fairfieldprep.org>
2022-10-19 12:12:01 -0400

    fixed typo

--- lab_4-8.py
+++ lab_4-8.py
@@ -1,4 +1,4 @@
 # Creator JM 10/19/22
-compliments = {'a':'t','t':'a','c':'g','g':'c'}; series = [] # Define compliemnents dictionary and list
+compliments = {'a':'t','t':'a','c':'g','g':'c'}; series = [] # Define compliments dictionary and list
 for x in range(0,6): series.append(compliments[input('A, C, T, or G?: ').lower()]) # Get all the letters
 print(" ".join([str(x) for x in series])) # Print the joined list
